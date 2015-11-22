77152cb776ea8f569a82cc1fd181064667ca3e84
coh <vaggian1992@gmail.com>
2015-11-22 02:31:25 +0200

    Added Retrofit Login Authentication Token

--- app/src/main/java/com/linos/android/clownfiesta/LoginSys.java
+++ app/src/main/java/com/linos/android/clownfiesta/LoginSys.java
@@ -3,8 +3,6 @@ package com.linos.android.clownfiesta;
 import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.transition.Fade;
 import android.transition.TransitionManager;
@@ -12,9 +10,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.Headers;
+import retrofit.http.POST;
+
 
 public class LoginSys extends Activity{
-
+    Activity LoginActivity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,4 +48,43 @@ public class LoginSys extends Activity{
                         .commit();
         */
     }
+
+    public void LoginBtn(View view){
+        //Toast.makeText(getActivity(), "YOO", Toast.LENGTH_SHORT).show();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://83.212.119.253:8000")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        BasicAuthToken BasicAuthToken = retrofit.create(BasicAuthToken.class);
+        Call<TokenCS> call = BasicAuthToken.login("admin","linoss123");
+        call.enqueue(new Callback<TokenCS>() {
+            @Override
+            public void onResponse(Response<TokenCS> response, Retrofit retrofit) {
+                String str = response.code() + " " + response.body().toString();
+                System.out.println(str);
+                Toast.makeText(LoginActivity, str , Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                t.printStackTrace();
+                Toast.makeText(LoginActivity, "Login Failed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public interface BasicAuthToken{
+        @FormUrlEncoded
+        @POST("api-token-auth/")
+        Call<TokenCS> login(@Field("username") String username,@Field("password") String password);
+    }
+    class TokenCS {
+        String token;
+        public String toString() {
+            return (token);
+        }
+    }
+
+
 }
